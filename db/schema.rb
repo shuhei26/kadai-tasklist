4d2c23dd0b23f35a16b4b19c45f44fe6d3616830
shuei.otsuka <shueheichopin@gmail.com>
2020-09-23 13:37:54 +0000

    tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2020_09_22_010624) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
     t.string "status"
   end
 
