5ded1f1e90512488de24549fe37fb2fa544b6efe
shuei.otsuka <shueheichopin@gmail.com>
2020-10-05 13:15:52 +0000

    complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :edit, :update, :destroy]
 
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(10)
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :create]
   resources :tasks
 end
